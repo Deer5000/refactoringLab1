84e875a3e513fce9e8209b69d036a47eb41f9925
Deer5000 <khidrbrinkley32@gmail.com>
2021-02-10 15:09:42 -0500

    exercise 5 complete

--- lab/refactoring/extract_method3.py
+++ lab/refactoring/extract_method3.py
@@ -1,20 +1,25 @@
 # Written by Kamran Bigdely
 # Example for Compose Methods: Extract Method.
 import math
-xc1 = 4
-yc1 = 4.25
+def distance_between():
+    circle1X = 4
+    circle1Y = 4.25
 
-xc2 = 53
-yc2 = -5.35
-# Calculate the distance between the two circle
-distance = math.sqrt((xc1-xc2)**2 + (yc1 - yc2)**2)
-print('distance', distance)
+    circle2X = 53
+    circle2Y = -5.35
+    # Calculate the distance between the two circle
+    distance = math.sqrt((circle1X-circle2X)**2 + (circle1Y - circle2Y)**2)
+    print('distance', distance)
 # *** somewhere else in your program ***
-xa = -36
-ya = 97
+def calculate_length():
+    xOfPointA = -36
+    yOfPointA = 97
 
-xb = .34
-yb = .91
-# calcualte the length of vector AB vector which is a vector between A and B points.
-length = math.sqrt((xa-xb)*(xa-xb) + (ya-yb)*(ya-yb))
-print('length', length)
+    xOfPointB = .34
+    yOfPointB = .91
+    # calcualte the length of vector AB vector which is a vector between A and B points.
+    length = math.sqrt((xOfPointA-xOfPointB)*(xOfPointA-xOfPointB) + (yOfPointA-yOfPointB)*(yOfPointA-yOfPointB))
+    print('length', length)
+
+distance_between()
+calculate_length()
